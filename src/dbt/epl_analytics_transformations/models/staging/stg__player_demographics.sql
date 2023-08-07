867e0deedd0902ebbbece1aac26e02a8b9c1fb81
masupa <dmasupa4@gmail.com>
2023-08-07 20:12:01 +0200

    update: dbt setup with basic models

---
+++ src/dbt/epl_analytics_transformations/models/staging/stg__player_demographics.sql
@@ -0,0 +1,19 @@
+with player_demographics as (
+
+    select
+        Player_Names as player_names,
+        Club as club,
+        Nationality as nationality,
+        Age as age,
+        Position as playing_position,
+        Prefered_Foot as prefered_playing_foot,
+        Height as height,
+        Weight as weight,
+        Previous_Teams as previous_teams,
+        DOB as date_of_birth,
+        Season as season
+
+    from {{ source('epl_analytics_dwh', 'player_demo_data') }}
+)
+
+select * from player_demographics
